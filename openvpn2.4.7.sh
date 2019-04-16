6f70a6815698c6bed56178bd556417f611ed9948
lcq3322 <lcq.3322@163.com>
2019-04-16 10:45:21 +0800

    Create openvpn2.4.7.sh

---
+++ openvpn2.4.7.sh
@@ -0,0 +1,50 @@
+#iip=`ip add | grep 'inet 192.168.242.128'`
+
+#if [ ! $iip ] ; then
+#   echo "1|环境错误! 请在OfficVpn.Sto运行"
+#   exit 1;
+#fi
+#yum install -y expect
+LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+PATH="$PATH:/sbin:/usr/sbin:/usr/local/sbin:/usr/bin:/bin:/usr/local/bin"
+export LANG LC_ALL PATH
+
+uuu=$1
+if [ ! $uuu ];then
+     echo '1|Please Use "sh vpnmail.sh username"'
+else
+test -e /etc/openvpn/easy-rsa/pki/issued/$uuu.crt
+  if [ $? -eq 0 ]; then
+echo "file  exist !!!"
+function sendmail () {
+rm -rf /data/scripts/files/$uuu
+mkdir -p /data/scripts/files/$uuu
+cp /etc/openvpn/easy-rsa/pki/ca.crt               /data/scripts/files/$uuu
+cp /etc/openvpn/easy-rsa/pki/issued/$uuu.crt      /data/scripts/files/$uuu
+cp /etc/openvpn/easy-rsa/pki/private/$uuu.key     /data/scripts/files/$uuu
+sleep 1
+cp /data/scripts/files/ddd.ovpn    /data/scripts/files/$uuu/stovpn.ovpn
+sed -i "s/ddd.crt/$uuu.crt/g"  /data/scripts/files/$uuu/stovpn.ovpn
+sed -i "s/ddd.key/$uuu.key/g"  /data/scripts/files/$uuu/stovpn.ovpn
+sleep 1
+cd /data/scripts/files/
+tar zcf  stovpn.tar.gz  $uuu  openvpn-install-2.4.7-I601.exe  && mv stovpn.tar.gz  $uuu
+sleep 2
+#mail -a /data/scripts/files/$uuu/stovpn.tar.gz -s VPN权限开通 $uuu@sto.cn < /data/scripts/files/content.txt  && echo "0|jobs done!!!"
+}
+sendmail
+  else
+#cd /etc/openvpn/easy-rsa && source ./vars
+cd /etc/openvpn/easy-rsa
+#source vars
+/usr/bin/expect << EOF
+#spawn sh build-key $uuu
+spawn sh easyrsa build-client-full $uuu  nopass
+expect "Enter pass phrase for /etc/openvpn/easy-rsa/pki/private/ca.key:"
+send "sto.ovpn\n"
+expect EOF
+EOF
+sendmail
+  fi
+fi
